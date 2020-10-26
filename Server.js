e8b8cc4ba13140fb630eb1ee1d07f6f491b5c828
Taaha <azadsumaiya00@gmail.com>
2020-10-26 09:20:38 +0600

    Completed

--- Server.js
+++ Server.js
@@ -233,11 +233,33 @@ app.get('/showGrade',async function(req,res){
     console.log(req.query);
     let query="select course_id,(select course_title from courses c where c.course_id=r.course_id) course_title,(select credit_hour from courses c where c.course_id=r.course_id) credit_hour,(select grade from grades g where g.grade_point=r.obtained_grade_point) obtained_grade,obtained_grade_point from registration r where student_id="+req.query.id+"and term_id='"+req.query.term_id+"'";
     //console.log(query);
+    let query2="select  sum(c.credit_hour) registered_credit_hours from registration r join courses c on(r.course_id=c.course_id) where r.student_id="+req.query.id+"and r.term_id='"+req.query.term_id+"'";
+    let query3="select  sum(c.credit_hour) earned_credit_hours from registration r join courses c on(r.course_id=c.course_id) where r.student_id="+req.query.id+"and r.term_id='"+req.query.term_id+"' and r.obtained_grade_point<>0";
+    let query4="select  sum(c.credit_hour) total_credit_hours from registration r join courses c on(r.course_id=c.course_id) where r.student_id="+req.query.id+" and r.obtained_grade_point<>0";
+    let query5="select  sum(c.credit_hour*r.obtained_grade_point) gpa  from registration r join courses c on(r.course_id=c.course_id) where r.student_id="+req.query.id+"and r.term_id='"+req.query.term_id+"'";
+    let query6="select  sum(c.credit_hour*r.obtained_grade_point) cgpa  from registration r join courses c on(r.course_id=c.course_id) where r.student_id="+req.query.id;
     try{
         const result=await connection.execute(query);
         console.log("The results found in query: ");
         console.log(result);
         console.log("returned rows from query: ",result.rows.length);
+        const result2=await connection.execute(query2);
+        console.log(result2.rows[0].REGISTERED_CREDIT_HOURS);
+        result.registered_credit_hours=result2.rows[0].REGISTERED_CREDIT_HOURS;
+        const result3=await connection.execute(query3);
+        console.log(result3.rows[0].EARNED_CREDIT_HOURS);
+        result.earned_credit_hours=result3.rows[0].EARNED_CREDIT_HOURS;
+        const result4=await connection.execute(query4);
+        console.log(result4.rows[0].TOTAL_CREDIT_HOURS);
+        result.total_credit_hours=result4.rows[0].TOTAL_CREDIT_HOURS;
+        const result5=await connection.execute(query5);
+        console.log(result5.rows[0].GPA);
+        result.gpa=result5.rows[0].GPA/result.earned_credit_hours;
+        console.log(result.gpa);
+        const result6=await connection.execute(query6);
+        console.log(result6.rows[0].CGPA);
+        result.cgpa=result6.rows[0].CGPA/result.total_credit_hours;
+        console.log(result.cgpa);
         res.send(result);
     }catch(e){
         console.log(e);
@@ -262,6 +284,11 @@ app.get('/registration',async function(req,res){
             console.log("The results found in query: ");
             result2.registration=true;
             console.log(result2);
+            let query3="select sum(c.credit_hour) total_credit_hour from courses c join courseinterm  t on(c.course_id=t.course_id) where t.term_id='"+req.query.term_id+"' and available_dept='"+req.query.available_dept+"'";
+            console.log(query3);
+            const result3=await connection.execute(query3);
+            console.log(result3.rows[0].TOTAL_CREDIT_HOUR);
+            result2.total_credit_hour=result3.rows[0].TOTAL_CREDIT_HOUR;
             return res.send(result2);
         }
         console.log("The results found in query: ");
@@ -276,14 +303,22 @@ app.get('/registration',async function(req,res){
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 app.get('/registrationApproval',async function(req,res){
-    console.log(req.query);
+    console.log("registrationApproval ",req.query);
     let query="select course_id,(select course_title from courses c where c.course_id=r.course_id) course_title,(select credit_hour from courses c where c.course_id=r.course_id) credit_hour from registration r where student_id="+req.query.id+"and term_id='"+req.query.term_id+"'";
     //console.log(query);
+    let query2="select  sum(c.credit_hour) registered_credit_hours from registration r join courses c on(r.course_id=c.course_id) where r.student_id="+req.query.id+"and r.term_id='"+req.query.term_id+"'";
+    let query3="select  sum(c.credit_hour) credit_hours_earned from registration r join courses c on(r.course_id=c.course_id) where r.student_id="+req.query.id+" and r.term_id<>'"+req.query.term_id+"'";
     try{
         const result=await connection.execute(query);
         console.log("returned rows from query: ",result.rows.length);
         console.log("The results found in query: ");
         console.log(result);
+        const result2=await connection.execute(query2);
+        console.log(result2.rows[0].REGISTERED_CREDIT_HOURS);
+        result.registered_credit_hours=result2.rows[0].REGISTERED_CREDIT_HOURS;
+        const result3=await connection.execute(query3);
+        console.log(result3.rows[0].CREDIT_HOURS_EARNED);
+        result.credit_hours_earned=result3.rows[0].CREDIT_HOURS_EARNED;
         res.send(result);
     }catch(e){
         console.log(e);
@@ -310,14 +345,4 @@ app.post('/insertRegistration',async function (req, res) {
         }
     }
     res.status(200).send();
-    // let query="insert into regions values("+req.body.id+",'"+req.body.name+"')";
-    // console.log(query);
-    // const result = await connection.execute(query);
-    // console.log("The results found in query: ");
-    // console.log(result);
-    // const result2 = await connection.execute(
-    //     `commit`,
-    // );
-    // console.log('The result found from coomit: ',result2);
-    // res.send(result);
 });
